ec5065f36bad6c120c431340f26f206af8c90e2d
fmurphy06 <murphy.fi@northeastern.edu>
2025-04-26 15:22:02 -0400

    Got Recursive functions working, and added boolean logic and number equality. Also added a rudimentary type system.

--- src/lela.rs
+++ src/lela.rs
@@ -1,16 +1,16 @@
 use std::collections::HashMap;
+// Illegal Expressions can be created. Since this is a dynamically typed language,
+// types are only important once evaluation occurs.
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Operator {
     Add,
     Subtract,
     Multiply,
     Divide,
-}
-
-pub struct Program {
-    global_scope: Scope,
-    expressions: Vec<Box<Expression>>,
+    And,
+    Or,
+    Equals
 }
 
 #[derive(Debug, Clone)]
@@ -39,24 +39,45 @@ pub struct Scope {
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value {
     Number(String),
-    String(String)
+    String(String),
+    Boolean(String),
 }
 
+#[derive(Debug)]
+pub enum LelaError {
+    EvaluationError(String),
+    SyntaxError(String),
+}
 
-// Defines a function that takes two Values of the same type. 
+fn parse_boolean(val: &String) -> Result<bool, LelaError> {
+    match val.as_str() {
+        "#true" => Ok(true),
+        "#false" => Ok(false),
+        _ => Err(LelaError::EvaluationError(format!("{:?} is not a valid boolean", val.clone()).to_string())),
+    }
+}
+
+fn same_type(val_a: &Value, val_b: &Value) -> bool {
+    match (val_a, val_b) {
+        (Value::Number(_), Value::Number(_)) => true,
+        (Value::String(_), Value::String(_)) => true,
+        (Value::Boolean(_), Value::Boolean(_)) => true,
+        (_, _) => false,
+    }
+}
+
+// Defines a function that takes two Values of the same type.
 macro_rules! define_uniform_value_function {
     ($func_name:ident, $first_type:pat, $sec_type:pat, $to_do:block) => {
-        fn $func_name(val_a: Value, val_b: Value) -> Value {
-            use Value::*;
+        fn $func_name(val_a: &Value, val_b: &Value) -> Value {
             match (val_a, val_b) {
                 ($first_type, $sec_type) => $to_do,
                 // TODO: this error message sucks
-                (_, _) => panic!("")
+                (_, _) => panic!(""),
             }
         }
     };
 }
-
 
 define_uniform_value_function!(add_numbers, Value::Number(a), Value::Number(b), {
     Value::Number((a.parse::<i32>().unwrap() + b.parse::<i32>().unwrap()).to_string())
@@ -74,53 +95,65 @@ define_uniform_value_function!(divide_numbers, Value::Number(a), Value::Number(b
     Value::Number((a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap()).to_string())
 });
 
-#[derive(Debug, Clone)]
-// An Expression is a tree of operations as branches and values as leaves.
-// The atomic types are numbers and identifiers so far.
+define_uniform_value_function!(and_booleans, Value::Boolean(a), Value::Boolean(b), {
+    Value::Boolean((parse_boolean(&a).unwrap() && parse_boolean(&b).unwrap()).to_string())
+});
+
+define_uniform_value_function!(or_booleans, Value::Boolean(a), Value::Boolean(b), {
+    Value::Boolean((parse_boolean(&a).unwrap() || parse_boolean(&b).unwrap()).to_string())
+});
+
+define_uniform_value_function!(equals_booleans, Value::Boolean(a), Value::Boolean(b), {
+    Value::Boolean((parse_boolean(&a).unwrap() == parse_boolean(&b).unwrap()).to_string())
+});
+define_uniform_value_function!(equals_number, Value::Number(a), Value::Number(b), {
+    Value::Boolean(format!("#{}", (a.parse::<i32>().unwrap() == b.parse::<i32>().unwrap())).to_string())
+});
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UnaryOperator {
+    Negate,
+    Not,
+    SquareRoot,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+/// An Expression is a tree of operations as branches and values as leaves.
 pub enum Expression {
     ValueExpr(Value),
     Identifier(String),
     List(Vec<Box<Expression>>),
     Operation(Operator, Box<Expression>, Box<Expression>),
+    UnaryOperation(UnaryOperator, Box<Expression>),
+    ConditionalTree(Vec<Box<Expression>>, Vec<Box<Expression>>), // must be the same length
     FunctionCall(String, Vec<Box<Expression>>),
 }
 
-pub fn filter_errors<'a>(
-    entries: Vec<
-        Result<
-            Box<ProgramEntry>,
-            lalrpop_util::ParseError<usize, lalrpop_util::lexer::Token<'a>, &'a str>,
-        >,
-    >,
-) -> Vec<Box<ProgramEntry>> {
-    entries
-        .into_iter()
-        .filter_map(|entry| match entry {
-            Ok(x) => Some(x),
-            Err(_) => None,
-        })
-        .collect()
-}
-
-pub fn evaluate_program(clean_program: &Vec<Box<ProgramEntry>>, scope: &mut Scope) -> Vec<Value> {
+pub fn evaluate_program(
+    clean_program: Vec<Result<Box<ProgramEntry>, LelaError>>,
+    scope: &mut Scope,
+) -> Vec<Result<Value, LelaError>> {
     let mut answers = Vec::new();
-    for entry in clean_program.iter() {
-        match entry.as_ref() {
-            ProgramEntry::Expression(expression) => {
-                answers.push(evaluate_expression(expression, scope));
+    for potential_entry in clean_program.into_iter() {
+        match potential_entry {
+            Ok(entry) => match entry.as_ref() {
+                ProgramEntry::Expression(expression) => {
+                    answers.push(evaluate_expression(&expression, scope));
+                }
+                ProgramEntry::Definition(definition) => match definition {
+                    Definition::ConstantDefinition(name, _) => {
+                        scope
+                            .definitions
+                            .insert(name.clone(), Box::new(definition.to_owned()));
+                    }
+                    Definition::FunctionDefinition(name, _, _) => {
+                        scope
+                            .definitions
+                            .insert(name.clone(), Box::new(definition.to_owned()));
+                    }
+                },
             }
-            ProgramEntry::Definition(definition) => match definition {
-                Definition::ConstantDefinition(name, _) => {
-                    scope
-                        .definitions
-                        .insert(name.clone(), Box::new(definition.to_owned()));
-                }
-                Definition::FunctionDefinition(name, _, _) => {
-                    scope
-                        .definitions
-                        .insert(name.clone(), Box::new(definition.to_owned()));
-                }
-            },
+            Err(e) => answers.push(Err(e))
         }
     }
     answers
@@ -132,20 +165,57 @@ fn evaluate_operation_expression(
     left: &Box<Expression>,
     right: &Box<Expression>,
     scope: &Scope,
-) -> Value {
+) -> Result<Value, LelaError> {
     match operator {
-        Operator::Add => add_numbers(evaluate_expression(left, scope), evaluate_expression(right, scope)),
-        Operator::Multiply => multiply_numbers(evaluate_expression(left, scope), evaluate_expression(right, scope)),
-        Operator::Divide => divide_numbers(evaluate_expression(left, scope), evaluate_expression(right, scope)),
-        Operator::Subtract => subtract_numbers(evaluate_expression(left, scope), evaluate_expression(right, scope)),
-    }
-}                    
+        Operator::Add => Ok(add_numbers(
+            &evaluate_expression(left, scope)?,
+            &evaluate_expression(right, scope)?,
+        )),
+        Operator::Multiply => Ok(multiply_numbers(
+            &evaluate_expression(left, scope)?,
+            &evaluate_expression(right, scope)?,
+        )),
+        Operator::Divide => Ok(divide_numbers(
+            &evaluate_expression(left, scope)?,
+            &evaluate_expression(right, scope)?,
+        )),
+        Operator::Subtract => Ok(subtract_numbers(
+            &evaluate_expression(left, scope)?,
+            &evaluate_expression(right, scope)?,
+        )),
+        Operator::And => {
+            Ok(and_booleans(
+                &evaluate_expression(left, scope)?,
+                &evaluate_expression(right, scope)?,
+            ))
+        }
+        Operator::Or => {
+            Ok(or_booleans(
+                &evaluate_expression(left, scope)?,
+                &evaluate_expression(right, scope)?,
+            ))
+        }
+        Operator::Equals => {
+            let left = evaluate_expression(left, scope)?;
+            let right = evaluate_expression(right, scope)?;
+            match (&left, &right) {
+                (Value::Number(_), Value::Number(_)) => Ok(equals_number(&left, &right)),
+                (Value::Boolean(_), Value::Boolean(_)) => Ok(equals_booleans(&left, &right)),
+                (_, _) => Err(LelaError::EvaluationError("Cannot perform equals on these two types".to_string()))
+            }
 
-fn evaluate_identifier_expression(identifier_name: &String, scope: &Scope) -> Value {
+        }
+    }
+}
+
+fn evaluate_identifier_expression(
+    identifier_name: &String,
+    scope: &Scope,
+) -> Result<Value, LelaError> {
     match scope.definitions.get(identifier_name.as_str()) {
         Some(def) => match def.as_ref() {
-            Definition::ConstantDefinition(name, expression) => {
-                evaluate_expression(&expression, scope)
+            Definition::ConstantDefinition(_name, expression) => {
+                Ok(evaluate_expression(&expression, scope)?)
             }
             Definition::FunctionDefinition(_, _, _) => {
                 unreachable!("This identifer is somehow evaluating to a function")
@@ -159,7 +229,7 @@ fn evaluate_function_call(
     identifier: &String,
     parameters: &Vec<Box<Expression>>,
     scope: &Scope,
-) -> Value {
+) -> Result<Value, LelaError> {
     let function = scope.definitions.get(identifier);
     match function {
         Some(definition) => evaluate_function_call_with_definition(definition, parameters, scope),
@@ -171,11 +241,11 @@ fn evaluate_function_call_with_definition(
     function_def: &Definition,
     given_parameters: &Vec<Box<Expression>>,
     scope: &Scope,
-) -> Value {
+) -> Result<Value, LelaError> {
     match function_def {
         Definition::ConstantDefinition(name, _) => panic!("{} is not a function!", name),
-        Definition::FunctionDefinition(name, def_parameters, expr) => {
-            if (def_parameters.len() != given_parameters.len()) {
+        Definition::FunctionDefinition(_name, def_parameters, expr) => {
+            if def_parameters.len() != given_parameters.len() {
                 panic!(
                     "Expected {} parameters, given {}",
                     def_parameters.len(),
@@ -186,12 +256,12 @@ fn evaluate_function_call_with_definition(
 
             for (i, param) in given_parameters.iter().enumerate() {
                 let new_param_name = def_parameters.get(i).unwrap();
-                let new_expression = param.as_ref().clone();
+                let new_expression = evaluate_expression(param, scope)?;
                 function_scope.definitions.insert(
                     new_param_name.clone(),
                     Box::new(Definition::ConstantDefinition(
                         new_param_name.clone(),
-                        Box::new(new_expression),
+                        Box::new(Expression::ValueExpr(new_expression)),
                     )),
                 );
             }
@@ -201,16 +271,77 @@ fn evaluate_function_call_with_definition(
 }
 
 // Evaluates a given expression with a given scope
-pub fn evaluate_expression(expr: &Box<Expression>, scope: &Scope) -> Value {
+pub fn evaluate_expression(expr: &Box<Expression>, scope: &Scope) -> Result<Value, LelaError> {
     match expr.as_ref() {
-        Expression::ValueExpr(n) => return n.clone(),
+        Expression::ValueExpr(n) => Ok(n.clone()),
         Expression::Operation(op, left, right) => {
-            return evaluate_operation_expression(&op, left, right, scope);
+            evaluate_operation_expression(&op, left, right, scope)
         }
-        Expression::Identifier(name) => evaluate_identifier_expression(name, scope),
-        Expression::List(list) => panic!("list"),
+        Expression::Identifier(name) => Ok(evaluate_identifier_expression(name, scope)?),
+        Expression::List(_list) => panic!("list"),
         Expression::FunctionCall(identifier, parameters) => {
             evaluate_function_call(identifier, parameters, scope)
         }
+        Expression::ConditionalTree(cases, values) => evaluate_conditional(cases, values, scope),
+        Expression::UnaryOperation(op, expr) => {
+            evaluate_unary_operation(op, &evaluate_expression(expr, scope)?)
+        }
+    }
+}
+
+fn evaluate_unary_operation(operation: &UnaryOperator, val: &Value) -> Result<Value, LelaError> {
+    match operation {
+        UnaryOperator::Negate => match val {
+            Value::Number(n) => Ok(Value::Number((-1 * n.parse::<i32>().unwrap()).to_string())),
+            _ => Err(LelaError::EvaluationError(
+                "Cannot negate a non-number".to_string(),
+            )),
+        },
+        UnaryOperator::Not => match val {
+            Value::Boolean(b) => Ok(Value::Boolean((!(parse_boolean(b).unwrap())).to_string())),
+            _ => Err(LelaError::EvaluationError(
+                "Cannot perform a not operations on a non-boolean value".to_string(),
+            )),
+        },
+        UnaryOperator::SquareRoot => match val {
+            Value::Number(n) => Ok(Value::Number(n.parse::<i32>().unwrap().to_string())),
+            _ => Err(LelaError::EvaluationError(
+                "Cannot perform a square root on a non-number value".to_string(),
+            )),
+        },
+    }
+}
+
+fn evaluate_conditional(
+    cases: &Vec<Box<Expression>>,
+    values: &Vec<Box<Expression>>,
+    scope: &Scope,
+) -> Result<Value, LelaError> {
+    let mut answer: Result<Value, LelaError> = Err(LelaError::EvaluationError(
+        "Nothing in this program".to_string(),
+    ));
+    if cases.len() == values.len() {
+        for (i, cond) in cases.iter().enumerate() {
+            match evaluate_expression(cond, scope) {
+                Ok(Value::Boolean(val)) => match parse_boolean(&val) {
+                    Ok(b) => {
+                        if b {
+                            answer = evaluate_expression(&values[i], scope);
+                            break;
+                        }
+                    }
+                    Err(e) => answer = Err(e)
+                },
+                Err(e) => panic!("{:?}", e),
+
+                _ => {}
+            }
+        }
+        match answer {
+            Ok(answer) => Ok(answer),
+            Err(e) => Err(e),
+        }
+    } else {
+        answer
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,76 @@
 use crate::lela_grammar::DefinitionOrExpressionParser;
-use lalrpop_util::lalrpop_mod;
-use lela::Expression;
+use lalrpop_util::{lalrpop_mod};
 use lela::ProgramEntry;
-use lela::evaluate_expression;
 use lela::Value;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
+use crate::lela::{evaluate_expression, evaluate_program, Expression, LelaError, Operator};
+
 pub mod lela;
 
 lalrpop_mod!(pub lela_grammar); // synthesized by LALRPOP
+
+/// Reads the contents of a file, given its relative path.
+fn read_from_file(file_path: &String) -> String {
+    // TODO: change to a match expression:
+    fs::read_to_string(file_path).expect("Should have been able to read the file")
+}
+
+/// Splits a given string on how a program should be split.
+fn split_as_program(src: &String) -> Vec<String> {
+    src.split(";").map(|s| s.to_string()).collect()
+}
+
+/// Given a file path, read it as a program
+fn read_program_from_file(file_path: &String) -> Vec<String> {
+    let source = read_from_file(file_path);
+    let split_file_input = split_as_program(&source);
+    let contents: Vec<String> = split_file_input;
+    contents
+}
+
+/// Produces parsed ASTs given a vector of strings
+fn parse_program_strings(
+    source: &Vec<String>,
+) -> Vec<
+    Result<
+        Box<ProgramEntry>,
+        lalrpop_util::ParseError<usize, lalrpop_util::lexer::Token<'_>, &str>,
+    >,
+> {
+    let parser = DefinitionOrExpressionParser::new();
+    let output = source
+        .iter()
+        .map(|input_line| parser.parse(input_line))
+        .collect();
+    output
+}
+
+/// Creates an empty scope.
+fn create_empty_scope() -> lela::Scope {
+    let global_scope = lela::Scope {
+        definitions: HashMap::new(),
+    };
+    global_scope
+}
+
+/// Given a Vector of Strings, evaluate each as a lela program.
+fn run_program(program: &Vec<String>) {
+    let mut global_scope = create_empty_scope();
+    let cleaned_program = parse_program_strings(program).into_iter().map(|entry|
+        match entry {
+            Ok(r) => Ok(r),
+            Err(e) => Err(LelaError::SyntaxError(e.to_string()))
+        }).collect();
+    // Create answers from our list of expressions
+    let answers = lela::evaluate_program(cleaned_program, &mut global_scope);
+    // Prints the evaluations
+    for ans in answers {
+        println!("{:?}", ans)
+    }
+}
+
 
 #[test]
 fn test_valid_parsings() {
@@ -21,7 +82,7 @@ fn test_valid_parsings() {
         "this_is_a_function_call(these, are, parameters)",
         "[line,\n breaks, \n should, \n be, \n fine]",
     ];
-    let parser = lela_grammar::DefinitionOrExpressionParser::new();
+    let parser = DefinitionOrExpressionParser::new();
     for string in good_code.iter() {
         assert!(
             parser.parse(string).is_ok(),
@@ -32,15 +93,95 @@ fn test_valid_parsings() {
 }
 
 #[test]
+fn test_proper_conditional_parsing() {
+    let cases = vec![Box::new(Expression::ValueExpr(Value::Boolean("#true".to_string()))),
+                     Box::new(Expression::ValueExpr(Value::Boolean("#true".to_string())))];
+    let values = vec![Box::new(Expression::ValueExpr(Value::Number(15.to_string()))),
+                      Box::new(Expression::ValueExpr(Value::Number(0.to_string())))];
+    let five = Box::new(Expression::ConditionalTree(cases, values));
+
+    let cond1 = "if #true { 15 } else { 0 }";
+    let parser = DefinitionOrExpressionParser::new();
+    let parse_result = parser.parse(cond1).unwrap();
+
+    match parse_result.as_ref() {
+        ProgramEntry::Expression(expr) => {
+            assert_eq!(expr.clone(), five)
+        }
+        ProgramEntry::Definition(_) => {
+            panic!("Parsing should have returned an expression.")
+        }
+    }
+}
+
+#[test]
+fn test_valid_evaluations() {
+    let fifteen = Expression::ValueExpr(Value::Number(15.to_string()));
+    let two = Expression::ValueExpr(Value::Number(2.to_string()));
+    let one_and_a_half = Expression::ValueExpr(Value::Number(1.5.to_string()));
+    let also_one_and_a_half = Expression::ValueExpr(Value::Number("3/2".to_string()));
+    let hello = Expression::ValueExpr(Value::String("hello".to_string()));
+    let true_val = Expression::ValueExpr(Value::String("true".to_string()));
+    let false_val = Expression::ValueExpr(Value::Boolean("false".to_string()));
+
+    let thirty_added = Expression::Operation(Operator::Add,
+                                             Box::new(fifteen.clone()),
+                                             Box::new(fifteen.clone()));
+    let three_added = Expression::Operation(Operator::Add,
+                                            Box::new(one_and_a_half.clone()),
+                                            Box::new(also_one_and_a_half.clone()));
+    let test_scope = create_empty_scope();
+
+    assert_eq!(lela::evaluate_expression(&Box::new(thirty_added), &test_scope).unwrap(), Value::Number(30.to_string()));
+    assert_eq!(lela::evaluate_expression(&Box::new(three_added), &test_scope).unwrap(), Value::Number(3.to_string()));
+    assert_eq!(lela::evaluate_expression(&Box::new(also_one_and_a_half), &test_scope).unwrap(), Value::Number(1.5.to_string()));
+
+}
+
+
+
+#[test]
+fn test_parsing_conditional_expressions() {
+    let cond1 = "if #true { (10 + 5) } else { (0 + 0) }";
+    let parser = DefinitionOrExpressionParser::new();
+    match parser.parse(cond1).unwrap().as_ref() {
+        ProgramEntry::Expression(expr) => match evaluate_expression(expr, &create_empty_scope()) {
+            Ok(val) => assert_eq!(val, Value::Number(15.to_string())),
+            Err(e) => panic!("{:?}", e)
+        },
+        ProgramEntry::Definition(def) => panic!("The following is being parsed as a definition: {:?}", def),
+    }
+
+}
+
+#[test]
+fn test_recursive_program() {
+    let recursive_prog_string =
+        "func factorial(n) {
+            switch {
+                case (n == 1): (1),
+                    case #true: factorial((n - 1)),
+                }
+            };
+        factorial(2)";
+    let mut scope = create_empty_scope();
+    let binding = split_as_program(&recursive_prog_string.to_string());
+    let program = parse_program_strings(&binding);
+    let cleaned = clean_program(program);
+    evaluate_program(cleaned, &mut scope);
+
+}
+
+#[test]
 fn test_invalid_parsings() {
     let bad_code = [
         "?Fire",
         "50Fire",
         "[), (]",
         "(17 /- Fire?)",
-        "this_isnt_a_function_call[these, arent, parameters]",
+        "this_isnt_a_function_call[this, is, a, list, [not, parameters]]",
     ];
-    let parser = lela_grammar::DefinitionOrExpressionParser::new();
+    let parser = DefinitionOrExpressionParser::new();
     for string in bad_code.iter() {
         assert!(
             parser.parse(string).is_err(),
@@ -50,6 +191,7 @@ fn test_invalid_parsings() {
     }
 }
 
+// Tests that identifiers are properly evaluated in the given scope.
 #[test]
 fn test_identifier_evaluation() {
     // create a scope
@@ -70,80 +212,42 @@ fn test_identifier_evaluation() {
         Box::new(lela::Expression::Identifier("fifteen".to_string())),
     ));
 
-    assert_eq!(lela::evaluate_expression(&test_expression, &scope), Value::Number(20.to_string()));
-}
-
-// Reads the contents of a file, given its relative path.
-fn read_from_file(file_path: &String) -> String {
-    // TODO: change to a match expression:
-    fs::read_to_string(file_path).expect("Should have been able to read the file")
-}
-
-fn split_as_program(src: &String) -> Vec<String> {
-    src.split(";").map(|s| s.to_string()).collect()
-}
-
-// Reads a file and splits it as a program
-fn read_program_from_file(file_path: &String) -> Vec<String> {
-    let source = read_from_file(file_path);
-    let split_file_input = split_as_program(&source);
-    let contents: Vec<String> = split_file_input;
-    contents
-}
-
-// Produces parsed ASTs given a vector of strings
-fn parse_program_strings(
-    source: &Vec<String>,
-) -> Vec<
-    Result<
-        Box<ProgramEntry>,
-        lalrpop_util::ParseError<usize, lalrpop_util::lexer::Token<'_>, &str>,
-    >,
-> {
-    let parser = DefinitionOrExpressionParser::new();
-    let output = source
-        .iter()
-        .map(|input_line| parser.parse(input_line))
-        .collect();
-    output
+    assert_eq!(lela::evaluate_expression(&test_expression, &scope).unwrap(), Value::Number(20.to_string()));
 }
 
 #[test]
-fn test_program_reading() {
-    // let args: Vec<String> = env::args().collect();
-    // let file_path = &args[1];
-
+// Runs the sample_code.lela file
+fn test_sample_program() {
     let input = "sample_code.lela".to_string();
     let program_read = read_program_from_file(&input);
-    let mut global_scope = lela::Scope {
-        definitions: HashMap::new(),
-    };
-    let cleaned_program = lela::filter_errors(parse_program_strings(&program_read).into());
-    // Create answers from our expressions
-    let answers = lela::evaluate_program(&cleaned_program, &mut global_scope);
-    // Prints the evaluations
-    for ans in answers {
-        println!("{:?}", ans)
-    }
+
+    run_program(&program_read);
 }
 
+fn clean_program(parsed: Vec<Result<Box<ProgramEntry>, lalrpop_util::ParseError<usize, lalrpop_util::lexer::Token<'_>, &str>>>) -> Vec<Result<Box<ProgramEntry>, LelaError>> {
+    parsed.into_iter().map(|entry|
+        match entry {
+            Ok(r) => Ok(r),
+            Err(e) => Err(LelaError::SyntaxError(e.to_string()))
+        }).collect()
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let read = read_program_from_file(file_path);
     println!("{:?}", read);
-    let parsed = parse_program_strings(&read);
+    let mut parsed = parse_program_strings(&read);
     println!("{:?}", parsed);
 
     let mut global_scope = lela::Scope {
         definitions: HashMap::new(),
     };
 
-    let cleaned_program = lela::filter_errors(parsed.into());
+    let mut cleaned_program:Vec<Result<Box<ProgramEntry>, LelaError>> = clean_program(parsed);
 
     // Create answers from our expressions
-    let answers = lela::evaluate_program(&cleaned_program, &mut global_scope);
+    let answers = lela::evaluate_program(cleaned_program, &mut global_scope);
     // Prints the evaluations
     for ans in answers {
         println!("{:?}", ans)
